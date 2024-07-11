71c53acc92155ea622a6b7b6cbac3c4e663e8641
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-07-11 14:06:32 +0200

    estructura web

---
+++ web-opo-esque/src/app/components/Article.jsx
@@ -0,0 +1,11 @@
+import "./Article.css"
+
+export default function Article() {
+  return (
+    <>
+      <article className="Article">
+        <h1>Article</h1>
+      </article>
+    </>
+  )
+}
---
+++ web-opo-esque/src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../components/Header.css"
+
+export default function Header() {
+  return (
+    <>
+      <header className="Header">
+        <h1>HEADER</h1>
+      </header>
+    </>
+  )
+}
